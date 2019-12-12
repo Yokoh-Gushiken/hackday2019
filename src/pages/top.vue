1fdc3a4c221f8b2b48f9e8276ef0c3409d99bf9a
gashiyama <higashiyama_masahiro@cam-inc.co.jp>
2019-12-12 20:03:55 +0900

    環境一次完成二回目

--- src/pages/top.vue
+++ src/pages/top.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="top">
-        <h1>TOP</h1>
+        <h1>TOPggggg</h1>
         <router-link to="gucci">Go to gucci</router-link>
         <router-link to="yoko">Go to yoko</router-link>
     </div>
